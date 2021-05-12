84633d0e8fc24557cdde4a6c19cd052706c25895
oen142 <oen142@naver.com>
2021-05-12 16:04:56 +0900

    배치어플리케이션 배치 인 액션

---
+++ src/main/java/com/wani/springbatchinaction/SpringBatchInActionApplication.java
@@ -0,0 +1,13 @@
+package com.wani.springbatchinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchInActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchInActionApplication.class, args);
+    }
+
+}
