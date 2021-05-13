22ce854fdaf8d291ad0b91da57cde1efc6e285b6
oen142 <oen142@naver.com>
2021-05-13 22:57:46 +0900

    feat : 첫 배치 시작

---
+++ src/main/java/com/wani/springbatchinaction/config/BatchProcessingConfig.java
@@ -0,0 +1,10 @@
+package com.wani.springbatchinaction.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchProcessingConfig {
+
+}
