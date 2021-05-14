c5ae3b0ae3e5e831c7a028ad493114c8341dd60b
oen142 <oen142@naver.com>
2021-05-14 23:40:56 +0900

    feat : 스텝이 실패했을때 다른 스텝을 시작한다

---
+++ src/main/java/com/wani/springbatchinaction/job/StepNextConditionalJobConfiguration.java
@@ -0,0 +1,34 @@
+package com.wani.springbatchinaction.job;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class StepNextConditionalJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job stepNextConditionalJob() {
+        return jobBuilderFactory.get("stepNextConditionalJob")
+            .start(conditionalJobStep1())
+                .on("FAILED")
+            .to(conditionalJobStep3())
+
+    }
+
+    @Bean
+    public Step conditionalJobStep1() {
+        return null;
+    }
+
+}
